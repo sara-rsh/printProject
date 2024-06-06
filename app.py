c994b420d8100d3497aed9917ba5b22424883a12
MehdiBordbar81 <mehdibordbar24@gmail.com>
2024-06-06 16:09:57 +0330

    Add(SignUp_And_Login)

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+import psycopg2
+import json
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="ProjectDb",
+    user="postgres",
+    password="81139913"
+)
+
+cur = conn.cursor()
+
+@app.route("/signUp", methods=["POST"])
+def signUp_user():
+    
+    # import pdb; pdb.set_trace()
+    data = json.loads(request.data)
+    username = data["username"]
+    password = data["password"]
+    phonenumber =data["phoneNumber"]
+
+
+    
+    query = "INSERT INTO user_info (username, user_password, phone_number) VALUES (%s, %s, %s)"
+    cur.execute(query, (username, password, phonenumber))
+
+    conn.commit()
+
+    return jsonify({"message": "User registered successfully"})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    
+    data = json.loads(request.data)
+    phonenumber =data["phoneNumber"]
+    password =data["password"]
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM user_info WHERE phone_number = %s AND user_password = %s", (phonenumber, password))
+    user = cur.fetchone()
+    if user and user[2] == password:
+        return jsonify({"message": "Login successful"})
+    else:
+        return jsonify({"message": "Invalid phonenumber or password"}), 401
+
+
+
+
+# cur.close()
+# conn.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
