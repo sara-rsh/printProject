5ddfc33be1df46ec7005fc01f1b93b2856d5f1b6
sara-rsh <sararashedy2002.com>
2024-06-07 13:44:17 +0330

    add axios

--- src/context/apiContext2.js
+++ src/context/apiContext2.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const ProductsContext2 = createContext();
 
@@ -10,10 +11,30 @@ export const ProductsProvider2 = ({ children }) => {
   const [products2, setProducts2] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then(response => response.json())
-      .then(data => setProducts2(data))
-      .catch(error => console.error(error));
+    // fetch("http://localhost:5000/products")
+    // fetch("https://api.escuelajs.co/api/v1/products")
+    //   .then(response => response.json())
+    //   .then(data => setProducts2(data))
+    //   .catch(error => console.error(error));
+
+    // const fetchData = async () => {
+    //   const result = await fetch("http://localhost:5000/products");
+    //   const jsonResult = await result.json();
+    //   setProducts2(jsonResult);
+    // };
+    // fetchData();
+
+    const fetchData = () => {
+      axios
+        .get("http://localhost:5000/products")
+        .then((response) => {
+          setProducts2(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+    };
+    fetchData();
   }, []);
 
   return (
